6682157d9ca5c1541059e7ef38d834838e88992b
Nga Tran <nga.tran@it-development.com>
2019-12-30 22:49:29 +0700

    Add missing Solution code

--- src/main/java/com/media/assignment/Solution.java
+++ src/main/java/com/media/assignment/Solution.java
@@ -2,9 +2,30 @@ package com.media.assignment;
 
 public class Solution {
 	public static void main(String[] args) {
+		// Solution A1
 		Bird bird = new Bird();
 		bird.walk();
 		bird.fly();
 		bird.sing();
+
+		// Solution A2
+		Duck duck = new Duck();
+		duck.fly();
+		duck.say();
+		duck.swim();
+		duck.sing();
+
+		Chicken chicken = new Chicken();
+		chicken.fly();
+		chicken.say();
+		chicken.swim();
+		chicken.sing();
+
+		// Solution A3
+		Rooster rooster = new Rooster();
+		rooster.fly();
+		rooster.say();
+		rooster.swim();
+		rooster.sing();
 	}
 }
